17f85d55e7419997816fdc9b8737cb86bdbec719
Meshiest <gpg@reheatedcake.io>
2022-04-14 11:24:59 -0500

    add plugin global scope value

--- modules/demo.js
+++ modules/demo.js
@@ -6,13 +6,17 @@ module.exports = async (plugin, OMEGGA_UTIL) => {
     const player = Omegga.getPlayer(name);
     const color = player.getNameColor();
     const message = OMEGGA_UTIL.chat.sanitize(args.join(' '));
-    Omegga.broadcast(`"<i><b><color=\\"${color}\\">${name}</></> ${message}</>`);
+    Omegga.broadcast(
+      `"<i><b><color=\\"${color}\\">${name}</></> ${message}</>`
+    );
   });
 
   return {
     registeredCommands: ['me'],
     example() {
-      Omegga.broadcast('"run this with: <code>;this.modules.demo.example()</>"');
+      Omegga.broadcast(
+        '"run this with: <code>;this.modules.demo.example()</>"'
+      );
     },
   };
 };
--- omegga.plugin.js
+++ omegga.plugin.js
@@ -8,20 +8,28 @@ module.exports = class Eval {
 
   // load a module
   async loadModule(name) {
-    return this.modules[name] = await require('./modules/' + name + '.js')(this, global.OMEGGA_UTIL);
+    return (this.modules[name] = await require('./modules/' + name + '.js')(
+      this,
+      global.OMEGGA_UTIL
+    ));
   }
 
   // add a module to the ones that will be loaded
-  async addModule(name, load=false) {
-    await this.store.set('modules', (await this.store.get('modules') || []).concat([name]));
-    if (load)
-      return this.modules[name] = await this.loadModule(name);
+  async addModule(name, load = false) {
+    await this.store.set(
+      'modules',
+      ((await this.store.get('modules')) || []).concat([name])
+    );
+    if (load) return (this.modules[name] = await this.loadModule(name));
     return 'ok';
   }
 
   // add a module to the ones that will be loaded
   async removeModule(name) {
-    await this.store.set('modules', (await this.store.get('modules') || []).filter(m => m.name !== name));
+    await this.store.set(
+      'modules',
+      ((await this.store.get('modules')) || []).filter(m => m.name !== name)
+    );
     return 'ok';
   }
 
@@ -35,46 +43,48 @@ module.exports = class Eval {
     const registeredCommands = [];
     let loaded = 0;
 
-    for (const mod of (await this.store.get('modules') || [])) {
+    for (const mod of (await this.store.get('modules')) || []) {
       try {
         const out = await this.loadModule(mod);
         if (typeof out === 'object' && out.registeredCommands instanceof Array)
           registeredCommands.push(...out.registeredCommands);
 
         this.modules[mod] = out;
-        loaded ++;
+        loaded++;
       } catch (err) {
         console.error('error loading module', mod, '-', err);
       }
     }
 
-    if (loaded > 0)
-      console.info('Loaded', loaded, 'modules');
+    if (loaded > 0) console.info('Loaded', loaded, 'modules');
 
-    Omegga
-      .on('chat', async (name, message) => {
+    Omegga.on('chat', async (name, message) => {
+      try {
+        if (message[0] !== ';' && !message.startsWith('&scl;')) return;
+        const code = message.slice(message.startsWith('&scl;') ? 5 : 1);
+        const player = Omegga.getPlayer(name);
+        if (
+          this.config['only-authorized'] &&
+          !player.isHost() &&
+          !this.config['authorized-users'].some(p => player.id === p.id)
+        )
+          return;
+        console.info(name, 'eval:', code);
         try {
-          if (message[0] !== ';' && !message.startsWith('&scl;')) return;
-          const code = message.slice(message.startsWith('&scl;') ? 5 : 1);
-          const player = Omegga.getPlayer(name);
-          if (
-            this.config['only-authorized'] && !player.isHost() &&
-            !this.config['authorized-users'].some(p => player.id === p.id)
-          ) return;
-          console.info(name, 'eval:', code);
-          try {
-            const result = eval(code);
-            if (result instanceof Promise)
-              Omegga.broadcast(`"<code>${await result}</>"`);
-            else
-              Omegga.broadcast(`"<code>${result}</>"`);
-          } catch (e) {
-            Omegga.broadcast(`"<code><color=\\"ff5555\\">error: ${e.message || e}</></>"`);
-          }
+          global.plugin = this;
+          eval(code);
+          if (result instanceof Promise)
+            Omegga.broadcast(`"<code>${await result}</>"`);
+          else Omegga.broadcast(`"<code>${result}</>"`);
         } catch (e) {
-          console.log(e);
+          Omegga.broadcast(
+            `"<code><color=\\"ff5555\\">error: ${e.message || e}</></>"`
+          );
         }
-      });
+      } catch (e) {
+        console.log(e);
+      }
+    });
 
     return { registeredCommands };
   }
